902efb443aa32a37b92c046560921bce618f43e6
Tim Upchurch <mail@timup.ch>
2016-03-31 18:11:25 -0400

    switch project scaffold to yeoman angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name howgorithmApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the howgorithmApp
+ */
+angular.module('howgorithmApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name howgorithmApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the howgorithmApp
+ */
+angular.module('howgorithmApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
